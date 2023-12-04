d4c583b734f0c57c0470c4c0dda90e0546288ce6
Frank Imbruglia <imbruglia100@gmail.com>
2023-12-04 11:14:48 -0500

    new set up

---
+++ app/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState('');
+
+  // WebSocket connection setup goes here
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:3000');
+    socket.onopen = () => {
+      console.log('WebSocket connection established.');
+    };
+
+    socket.onmessage = (event) => {
+      const receivedMessage = JSON.parse(event.data);
+      setMessages([...messages, receivedMessage]);
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, [messages]);
+
+  const sendMessage = () => {
+    // Implement sending messages via WebSocket here
+      if (messageInput.trim() !== '') {
+        const message = {
+          text: messageInput,
+          timestamp: new Date().toISOString(),
+        };
+        socket.send(JSON.stringify(message));
+        setMessageInput('');
+      }
+  };
+
+  return (
+    <div className="App">
+      <div className="chat-container">
+        <div className="chat-messages">
+          {messages.map((message, index) => (
+            <div key={index} className="message">
+              {message}
+            </div>
+          ))}
+        </div>
+        <div className="chat-input">
+          <input
+            type="text"
+            placeholder="Type your message..."
+            value={messageInput}
+            onChange={(e) => setMessageInput(e.target.value)}
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
